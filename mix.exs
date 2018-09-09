6e51f69e1dab6f525c0c8d9346697b6141e03d40
mpraski <marcin.praski@live.com>
2018-09-09 11:15:57 +0100

    fixed version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FlowMonitor.MixProject do
     [
       app: :flow_monitor,
       version: "0.1.1",
-      elixir: "> 1.7.2",
+      elixir: ">= 1.7.2",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
